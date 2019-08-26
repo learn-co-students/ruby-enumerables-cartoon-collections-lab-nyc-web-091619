0b40d41b22910e4c326c80143c91ce77ced9e51f
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 02:27:57 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,25 +12,16 @@ def long_planeteer_calls(array)
   array.any? {|name| name.length>4}
 end
 
-# def find_the_cheese(array)
-  # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-#   if array.include?("cheddar")
-#     return "cheddar"
-#   elsif array.include?("gouda")
-#     return "gouda"
-#   elsif array.include?("camembert")
-#     return "camembert"
-#   else
-#     return nil
-#   end
-# end
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
-
-def find_the_cheese(potentially_cheesy_items)
-  cheeses = %w[gouda cheddar camembert]
-
-  potentially_cheesy_items.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?("cheddar")
+    return "cheddar"
+  elsif array.include?("gouda")
+    return "gouda"
+  elsif array.include?("camembert")
+    return "camembert"
+  else
+    return nil
   end
 end
+
