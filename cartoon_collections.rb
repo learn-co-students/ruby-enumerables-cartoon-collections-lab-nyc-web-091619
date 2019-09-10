4960d6459d2f8bb8eb754ae178de4862b11c105b
Ashley Karhoff <ashley.karhoff1@gmail.com>
2019-09-10 14:58:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |dwarve, index|
+    puts "#{index + 1}. #{dwarve}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+
+# array of strings. It should then
+# look through these strings to find and return the first string that is a type of
+# cheese. 
+# return nil if none are found, or cheese name 
+
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  ingredients.detect { |item| cheese_types.include?(item) }
 end
